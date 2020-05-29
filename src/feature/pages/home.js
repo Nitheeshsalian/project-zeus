e526029dfed452b9e7c47b0be243b02b97a342ed
nitheeshsalian <nitheeshsalian@gmail.com>
2020-05-29 12:27:59 +0530

    material theme added

--- src/feature/pages/home.js
+++ src/feature/pages/home.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
 
 export default function Home(){
+    const classes = useStyles();
     return(
-        <div>
-            <div>this is new page which is rendering</div>
-        </div>
+    //     <React.Fragment>
+    //     <CssBaseline />
+    //     <Container maxWidth="sm">
+    //       <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+    //     </Container>
+    //   </React.Fragment>
+    <div className={classes.root}>
+      <AppBar position="static" color='primary'>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Nitheesh
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
     )
 }
